e241c5426c4c19c49cad707cc7d00b645c6688e9
每天一点 <877617829@qq.com>
2023-05-14 00:27:14 +0800

    【优化】more-popup优化

--- pages/biz/user/more-popup.vue
+++ pages/biz/user/more-popup.vue
@@ -1,17 +1,15 @@
 <template>
-	<!-- :mask-click="false" -->
-	<uni-popup ref="popupRef" type="bottom" background-color="#fff" safeArea>
-		<uni-list :border="false">
-			<uni-list-item v-if="hasPerm('mobileBizUserEdit')" title="编辑" style="text-align:center; margin: 0 15px;" :clickable="true" @click="edit">
-				<!-- <template v-slot:body>
-					<text style="flex: 1;font-size: 14px; margin-right: 10px; color: #1cbbb4;" @click="edit">编辑</text>
-				</template> -->
-			</uni-list-item>
-			<uni-list-item v-if="hasPerm('mobileBizUserDelete')" title="刪除" style="text-align:center; margin: 0 15px;" :clickable="true" @click="del">
-			</uni-list-item>
-			<uni-list-item title="取消" style="text-align:center; margin: 0 15px;" :clickable="true" @click="cancel">
-			</uni-list-item>
-		</uni-list>
+	<uni-popup ref="popupRef" type="bottom" safeArea>
+		<view class="container">
+			<uni-list :border="false">
+				<uni-list-item v-if="hasPerm('mobileBizUserEdit')" title="编辑" class="item" :clickable="true" @click="edit">
+				</uni-list-item>
+				<uni-list-item v-if="hasPerm('mobileBizUserDelete')" title="刪除" class="item" :clickable="true" @click="del">
+				</uni-list-item>
+				<uni-list-item title="取消" class="item" :clickable="true" @click="cancel">
+				</uni-list-item>
+			</uni-list>
+		</view>
 	</uni-popup>
 </template>
 
@@ -75,3 +73,14 @@
 		open
 	})
 </script>
+<style lang="scss">
+	.container {
+		margin: 15upx;
+		border-radius: 5upx;
+		padding: 5upx;
+		background-color: $uni-white;
+		.item {
+			text-align:center;
+		}
+	}
+</style>
