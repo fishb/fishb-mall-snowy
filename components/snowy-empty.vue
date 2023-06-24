61dd70b0c8b252c44a10011b0d9e47b654217a6f
877617829 <877617829@qq.com>
2023-06-24 21:19:10 +0800

    【新增】空数据组件

---
+++ components/snowy-empty.vue
@@ -0,0 +1,28 @@
+<template>
+	<view class="empty-box">
+		<text>{{ tips }}</text>
+	</view>
+</template>
+
+<script setup>
+	const props = defineProps({
+		tips: {
+			type: String,
+			required: false,
+			default: "页面数据为空",
+		},
+	})
+</script>
+
+<style lang="scss" scoped>
+	.empty-box {
+		height: 800upx;
+		/* #ifndef APP-NVUE */
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		/* #endif */
+		color: #666666;
+		border: none;
+	}
+</style>
